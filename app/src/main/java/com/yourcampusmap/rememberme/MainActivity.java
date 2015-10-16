926c0f678e2cf3947e09f56e4fadb09d07168cfe
YourCampusMap <yourcampusmap@gmail.com>
2015-10-15 23:24:51 -0700

    update

--- app/src/main/java/com/yourcampusmap/rememberme/MainActivity.java
+++ app/src/main/java/com/yourcampusmap/rememberme/MainActivity.java
@@ -1,7 +1,12 @@
 package com.yourcampusmap.rememberme;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +16,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertNote("New note");
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, "New note");
+        Uri noteUri = getContentResolver().insert(NoteProvide.CONTENT_URI,
+                values);
+        Log.d("MainActivity", "Inserted note" + noteUri.getLastPathSegment());
     }
 
     @Override
